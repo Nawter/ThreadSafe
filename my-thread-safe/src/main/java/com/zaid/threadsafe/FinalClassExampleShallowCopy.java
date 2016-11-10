62ed7b1d7201977e6e67cf9fd7e28f93a54fd7fc
zaiddawoodissa <zaiddawoodissa@gmail.com>
2016-11-10 21:03:44 +0000

    added test of mutable class

---
+++ my-thread-safe/src/main/java/com/zaid/threadsafe/FinalClassExampleShallowCopy.java
@@ -0,0 +1,34 @@
+package com.zaid.threadsafe;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * FinalClassExampleShallowCopy
+ *
+ */
+public final class FinalClassExampleShallowCopy {
+
+	private final int id;
+	private final String name;
+	private final HashMap<String, String> map;
+
+	public FinalClassExampleShallowCopy(int id, String name, HashMap<String, String> hashMap) {
+		this.id=id;
+		this.name=name;
+		this.map=hashMap;
+	}
+
+	public String getName() {
+		return this.name;
+	}
+
+	public Map<String, String> getTestMap() {
+		return this.map; 
+	}
+
+	public int getId() {
+		return this.id;
+	}
+
+}
--- my-thread-safe/src/test/java/com/zaid/threadsafe/FinalClassExampleTest.java
+++ my-thread-safe/src/test/java/com/zaid/threadsafe/FinalClassExampleTest.java
@@ -8,10 +8,10 @@ import org.junit.Before;
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Unit test for FinalClassExampleTest
  */
 public class FinalClassExampleTest {
-	private FinalClassExample finalClassExample;
+	private FinalClassExampleShallowCopy finalClassExample;
 	private HashMap<String, String> hashMap;
 	private String name;
 	private int id;
@@ -19,17 +19,27 @@ public class FinalClassExampleTest {
 	@Before
 	public void setUp(){
 		hashMap = new HashMap<String, String>();
+		hashMap.put("1", "first");
+		hashMap.put("2", "second");
 		name="original";
 		id=10;
-		finalClassExample=new FinalClassExample(id,name,hashMap);
+		finalClassExample=new FinalClassExampleShallowCopy(id,name,hashMap);
 		
 	}
 
 	@Test
-	public void checkTheDeepCopyOfImmutableClass() {			
+	public void checkTheShallowCopyOfImmutableClass() {			
 		assertEquals(id,finalClassExample.getId());
 		assertEquals(name,finalClassExample.getName());
-		assertEquals(hashMap, finalClassExample.getTestMap());		
+		assertEquals(hashMap.size(), finalClassExample.getTestMap().size());	
+		// put new variables into the immutable class and see if its change so its not immutable.
+		id=20;		
+		name="modified";
+		hashMap.put("3", "third");
+		finalClassExample=new FinalClassExampleShallowCopy(id,name,hashMap);
+		assertEquals(id,finalClassExample.getId());		
+		assertEquals(name,finalClassExample.getName());
+		assertEquals(hashMap.size(), finalClassExample.getTestMap().size());
 	}
 
 }
